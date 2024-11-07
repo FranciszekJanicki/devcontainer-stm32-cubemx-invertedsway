672af3ae3c969642848d06b7b7fda37609c70e12
Franciszek Janicki <franciszek.janicki1@gmail.com>
2024-11-07 23:22:50 +0100

    move system_clock_config func to another file because it was driving me nuts, lol

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -5,6 +5,7 @@
 #include "l298n.hpp"
 #include "mpu6050.hpp"
 #include "regulator.hpp"
+#include "system_clock.h"
 #include "tim.h"
 #include "usart.h"
 #include <cstdio>
@@ -17,47 +18,6 @@ using namespace InvertedSway;
 
 constexpr MPU6050::Scaled ANGLE = 0;
 constexpr MPU6050::Scaled SAMPLING_TIME = 1;
-
-void SystemClock_Config(void)
-{
-    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-
-    /** Configure the main internal regulator output voltage
-     */
-    if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK) {
-        Error_Handler();
-    }
-
-    /** Initializes the RCC Oscillators according to the specified parameters
-     * in the RCC_OscInitTypeDef structure.
-     */
-    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-    RCC_OscInitStruct.PLL.PLLM = 1;
-    RCC_OscInitStruct.PLL.PLLN = 10;
-    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV7;
-    RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
-    RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
-    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
-        Error_Handler();
-    }
-
-    /** Initializes the CPU, AHB and APB buses clocks
-     */
-    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
-    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-
-    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK) {
-        Error_Handler();
-    }
-}
 
 // for using System object in callback
 System* system_handle{nullptr};
@@ -121,11 +81,6 @@ namespace BetterMain {
 
 }; // namespace BetterMain
 
-int main(const int argc, const char* argv[])
-{
-    return BetterMain::main<int>(BetterMain::Arguments{argv, argc});
-}
-
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef* htim)
 {
     if (htim == nullptr) {
@@ -164,6 +119,11 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef* htim)
 //         }
 //     }
 // }
+
+int main(const int argc, const char* argv[])
+{
+    return BetterMain::main<int>(BetterMain::Arguments{argv, argc});
+}
 
 void Error_Handler()
 {
