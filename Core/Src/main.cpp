333d2df41c218fd22307423e10e03f4bd7bcd58b
Franciszek Janicki <franciszek.janicki1@gmail.com>
2024-12-19 01:27:20 +0100

    change double to float as default type

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -14,40 +14,54 @@
 #include <cstdio>
 #include <utility>
 
-static bool timer_elapsed{false};
+using namespace InvertedSway;
+using namespace Regulators;
+using namespace Filters;
+using namespace Tests;
 
-static void balance_sway()
+void test_motor()
 {
-    using namespace InvertedSway;
-    using namespace Regulators;
-    using namespace Filters;
+    MX_GPIO_Init();
+    MX_USART2_UART_Init();
+    MX_TIM2_Init();
 
-    float const angle{0.0f};
-    float const sampling_time{MPU6050::SAMPLING_TIME_S};
+    MOTOR_TEST(Motor{&htim2, TIM_CHANNEL_1, GPIOB, IN1_Pin, IN3_Pin});
+}
 
-    L298N l298n{L298N::MotorChannels{
-        L298N::make_motor_channel(L298N::Channel::CHANNEL1, &htim2, TIM_CHANNEL_1, GPIOB, IN1_Pin, IN3_Pin),
-        L298N::make_motor_channel(L298N::Channel::CHANNEL2),
-    }};
+void test_motor_boost()
+{
+    MX_GPIO_Init();
+    MX_USART2_UART_Init();
+    MX_TIM2_Init();
 
-    MPU6050 mpu6050{&hi2c1, MPU6050::ADDRESS, MPU6050::GYRO_FS_250, MPU6050::ACCEL_FS_2, MPU6050::SAMPLING_RATE_HZ};
+    MOTOR_BOOST_TEST(Motor{&htim2, TIM_CHANNEL_1, GPIOB, IN1_Pin, IN3_Pin}, 2.0f);
+}
 
-    auto kalman{make_kalman(0.0f, 0.0f, 0.1f, 0.3f, 0.03f)};
+void test_encoder()
+{
+    MX_GPIO_Init();
+    MX_USART2_UART_Init();
+    MX_TIM1_Init();
+    MX_TIM2_Init();
 
-    auto regulator{make_regulator<Algorithm::PID>(0.0f, 0.0f, 0.0f, 0.0f)};
+    ENCODER_TEST(Encoder{&htim1}, Motor{&htim2, TIM_CHANNEL_1, GPIOB, IN1_Pin, IN3_Pin});
+}
 
-    Encoder encoder{&htim1};
+void test_kalman()
+{
+    MX_I2C1_Init();
+    MX_USART2_UART_Init();
 
-    System system{std::move(mpu6050), std::move(l298n), std::move(kalman), std::move(regulator), std::move(encoder)};
+    KALMAN_TEST(MPU6050{&hi2c1, MPU6050::ADDRESS, MPU6050::GYRO_FS_250, MPU6050::ACCEL_FS_2, MPU6050::SAMPLING_RATE_HZ},
+                make_kalman(0.0f, 0.0f, 0.1f, 0.3f, 0.03f),
+                MPU6050::SAMPLING_TIME_S);
+}
 
-    HAL_TIM_Base_Start_IT(&htim3);
+void test_dutkiewicz()
+{
+    MX_USART2_UART_Init();
 
-    while (true) {
-        if (timer_elapsed) {
-            system(angle, sampling_time);
-            timer_elapsed = false;
-        }
-    }
+    DUTKIEWICZ_TEST();
 }
 
 int main()
@@ -55,24 +69,10 @@ int main()
     HAL_Init();
     SystemClock_Config();
 
-    MX_GPIO_Init();
-    MX_USART2_UART_Init();
-    MX_I2C1_Init();
-    MX_TIM1_Init();
-    MX_TIM2_Init();
-    MX_TIM3_Init();
-
-    balance_sway();
+    /* most important test */
+    test_dutkiewicz();
 
     return 0;
-}
-
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef* htim)
-{
-    if (htim->Instance == TIM3) {
-        timer_elapsed = true;
-    }
-    HAL_TIM_Base_Start_IT(htim);
 }
 
 void Error_Handler()
