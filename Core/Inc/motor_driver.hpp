8790f0910ace587c5426d97c9282d44586a27e0e
Franciszek Janicki <franciszek.janicki1@gmail.com>
2024-12-23 20:13:13 +0100

    dodaj pliki projektu

--- Core/Inc/motor_driver.hpp
+++ Core/Inc/motor_driver.hpp
@@ -10,20 +10,8 @@ namespace InvertedSway {
 
     struct MotorDriver {
     public:
-        enum struct Error {
-            MOTOR_FAIL,
-            ENCODER_FAIL,
-            OK,
-            FAIL,
-            INIT,
-            DEINIT,
-        };
-
-        using Value = float;
-        using Direction = Motor::Direction;
+        using Value = Motor::Voltage;
         using Regulator = Regulators::Regulator<Value>;
-        using Expected = Encoder::ExpectedSpeed;
-        using Unexpected = Encoder::Unexpected;
 
         MotorDriver() noexcept = default;
 
@@ -37,24 +25,28 @@ namespace InvertedSway {
 
         ~MotorDriver() noexcept;
 
-        [[nodiscard]] Error
-        operator()(Direction const input_direction, Value const input_speed, Value const dt) noexcept;
+        [[nodiscard]] void operator()(Value const input_speed, Value const dt) noexcept;
 
     private:
-        static Direction speed_to_direction(Value const speed) noexcept;
+        static Value speed_to_voltage(Value const speed) noexcept;
+        static Value clamp_speed(Value const speed) noexcept;
 
-        Error set_speed(Value const control_speed) noexcept;
-        Error set_direction(Value const control_speed) noexcept;
+        static constexpr Value MAX_VOLTAGE_V{Motor::MAX_VOLTAGE_V};
+        static constexpr Value MIN_VOLTAGE_V{Motor::MIN_VOLTAGE_V};
 
-        Expected get_speed(Value const dt) noexcept;
+        static constexpr Value MIN_SPEED_RPM{0};
+        static constexpr Value MAX_SPEED_RPM{1000};
 
+        void set_voltage(Value const control_speed) const noexcept;
+        void set_speed(Value const control_speed) const noexcept;
+        void set_direction(Value const control_speed) const noexcept;
+
+        Value get_measured_speed(Value const dt) noexcept;
         Value get_control_speed(Value const error_speed, Value const dt) noexcept;
         Value get_error_speed(Value const input_speed, Value const dt) noexcept;
 
         void initialize() const noexcept;
         void deinitialize() const noexcept;
-
-        Direction last_direction_{};
 
         Regulator regulator_{};
         Motor motor_{};
--- Core/Src/system.cpp
+++ Core/Src/system.cpp
@@ -17,6 +17,16 @@ using Regulator = System::Regulator;
 
 namespace InvertedSway {
 
+    Value System::angle_to_voltage(Value const angle) noexcept
+    {
+        return SWAY_MASS_KG * EARTH_ACCELERATION * MOTOR_RESISTANCE * std::sin(angle) / MOTOR_VELOCITY_CONSTANT;
+    }
+
+    Value System::voltage_to_angle(Value const voltage) noexcept
+    {
+        return std::asin((voltage * MOTOR_VELOCITY_CONSTANT) / (SWAY_MASS_KG * EARTH_ACCELERATION * MOTOR_RESISTANCE));
+    }
+
     System::System(MPU6050&& mpu6050, L298N&& l298n, Kalman&& kalman, Regulator&& regulator, Encoder&& encoder) noexcept
         :
         mpu6050_{std::forward<MPU6050>(mpu6050)},
@@ -33,125 +43,95 @@ namespace InvertedSway {
         this->deinitialize();
     }
 
-    void System::balance_sway(Value const angle, Value const dt) noexcept
+    void System::operator()(Value const input_angle, Value const dt) noexcept
     {
-        this->update_dt(dt);
-        this->update_input_signal(angle);
-        this->update_output_signal();
-        this->update_error_signal();
-        this->update_control_signal();
-        this->update_direction();
-        this->update_compare();
+        this->set_angle(this->get_control_angle(this->get_error_angle(input_angle, dt), dt));
     }
 
-    void System::operator()(Value const angle, Value const dt) noexcept
-    {
-        this->balance_sway(angle, dt);
-    }
-
-    void System::update_dt(Value const dt) noexcept
-    {
-        this->dt_ = dt;
-        // printf("sampling time: %f", this->dt_);
-    }
-
-    void System::update_input_signal(Value const input_signal) noexcept
-    {
-        this->input_signal_ = input_signal;
-        // printf("input angle: %f\n\r", this->input_signal_);
-    }
-
-    void System::update_output_signal() noexcept
+    Value System::get_measured_angle(Value const dt) noexcept
     {
         if (HAL_GPIO_ReadPin(MPU6050_INTR_GPIO_Port, MPU6050_INTR_Pin) == GPIO_PinState::GPIO_PIN_SET) {
             this->roll_ = this->mpu6050_.get_roll();
             this->gx_ = this->mpu6050_.get_rotation_x_scaled();
         }
         // printf("mpu angle: %f, %f\n\r", this->gx_, this->roll_);
-
-        this->output_signal_ = this->kalman_(this->gx_, this->roll_, this->dt_);
         // printf("kalman angle: %f\n\r", this->output_signal_);
-
         // printf("encoder angle: %f\n\r", this->encoder_.get_angle().value());
+        return this->kalman_(this->gx_, this->roll_, dt);
     }
 
-    void System::update_error_signal() noexcept
+    Value System::get_error_angle(Value const input_angle, Value const dt) noexcept
     {
-        this->error_signal_ = this->input_signal_ - this->output_signal_;
-        // printf("error angle: %f\n\r", this->error_signal_);
+        return input_angle - this->get_measured_angle(dt);
     }
 
-    void System::update_control_signal() noexcept
+    Value System::get_control_angle(Value const error_angle, Value const dt) noexcept
     {
 #if defined(REGULATOR_PTR)
-
         if (this->regulator_ != nullptr) {
-            this->control_signal_ = std::invoke(*this->regulator_, this->error_signal_, this->dt_);
+            return std::invoke(*this->regulator_, this->error_signal_, this->dt_);
         }
+        std::unreachable();
 #elif defined(REGULATOR_VARIANT)
         if (!this->regulator_.valueless_by_exception()) {
-            this->control_signal_ = std::visit(
-                [this]<typename Regulator>(Regulator&& regulator) {
-                    return std::invoke(regulator, this->error_signal_, this->dt_);
-                },
-                this->regulator_);
+            return std::visit([error_angle, dt]<typename Regulator>(
+                                  Regulator&& regulator) { return std::invoke(regulator, error_angle, dt); },
+                              this->regulator_);
         }
+        std::unreachable();
 #elif defined(REGULATOR_LAMBDA)
         if (this->regulator_) {
-            this->control_signal_ = std::invoke(this->regulator_, this->error_signal_, this->dt_);
+            return std::invoke(this->regulator_, error_angle, dt);
         }
+        std::unreachable();
 #endif
-        // printf("regulated angle: %f\n\r", this->control_signal_);
     }
 
-    Value System::angle_to_voltage(Value const angle) noexcept
+    void System::set_angle(Value const control_angle) noexcept
     {
-        return SWAY_MASS_KG * EARTH_ACCELERATION * MOTOR_RESISTANCE * std::sin(angle) / MOTOR_VELOCITY_CONSTANT;
+        this->try_motor_boost(control_angle);
+        this->set_direction(control_angle);
+        this->set_voltage(control_angle);
     }
 
-    Value System::voltage_to_angle(Value const voltage) noexcept
+    void System::set_direction(Value const control_angle) const noexcept
     {
-        return std::asin((voltage * MOTOR_VELOCITY_CONSTANT) / (SWAY_MASS_KG * EARTH_ACCELERATION * MOTOR_RESISTANCE));
-    }
-
-    void System::update_direction() noexcept
-    {
-        if (this->error_signal_ >= 0) {
+        if (control_angle > 0.0f) {
             this->l298n_.set_forward(L298N::Channel::CHANNEL1);
             // printf("setting motor forward\n\r");
-        } else if (this->error_signal_ <= 0) {
+        } else if (control_angle < 0.0f) {
             this->l298n_.set_backward(L298N::Channel::CHANNEL1);
             // printf("setting motor backward\n\r");
         } else {
-            this->l298n_.set_fast_stop(L298N::Channel::CHANNEL1);
+            this->l298n_.set_soft_stop(L298N::Channel::CHANNEL1);
             // printf("setting motor stop\n\r");
         }
     }
 
-    void System::update_compare() noexcept
+    void System::set_voltage(Value const control_angle) const noexcept
     {
-        if (std::exchange(this->last_control_signal_, this->control_signal_) <
-                voltage_to_angle(MOTOR_START_THRESHOLD_V) &&
-            this->control_signal_ >= voltage_to_angle(MOTOR_START_THRESHOLD_V)) {
-            this->set_angle(voltage_to_angle(MAX_CONTROL_SIGNAL_V));
+        this->l298n_.set_compare_voltage(L298N::Channel::CHANNEL1, angle_to_voltage(std::abs(control_angle)));
+    }
+
+    void System::try_motor_boost(Value const control_angle) noexcept
+    {
+        if (std::exchange(this->last_control_voltage_, angle_to_voltage(control_angle)) < MOTOR_START_THRESHOLD_V &&
+            angle_to_voltage(control_angle) >= MOTOR_START_THRESHOLD_V) {
+            this->set_voltage(MAX_CONTROL_SIGNAL_V);
             HAL_Delay(10);
         }
-        this->set_angle(this->control_signal_);
     }
 
     void System::initialize() noexcept
     {
-        this->set_angle(0.0);
+        this->l298n_.set_fast_stop(L298N::Channel::CHANNEL1);
+        this->l298n_.set_compare_min(L298N::Channel::CHANNEL1);
     }
 
     void System::deinitialize() noexcept
     {
-        this->set_angle(0.0);
-    }
-
-    void System::set_angle(Value const angle) noexcept
-    {
-        this->l298n_.set_compare_voltage(L298N::Channel::CHANNEL1, angle_to_voltage(std::abs(angle)));
+        this->l298n_.set_fast_stop(L298N::Channel::CHANNEL1);
+        this->l298n_.set_compare_min(L298N::Channel::CHANNEL1);
     }
 
 }; // namespace InvertedSway
